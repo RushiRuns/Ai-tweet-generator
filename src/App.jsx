e4a937d4c7633e8ef7004e80c66ec495253b30ed
RushiRuns <rushikeshaundhakar715@gmail.com>
2025-12-27 17:03:31 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./components/navbar";
+import Page from "./components/page";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="">
+      <Navbar />
+      <Page />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/page.jsx
@@ -0,0 +1,15 @@
+import Create from "./create.jsx";
+import Favorite from "./favorite.jsx";
+import History from "./history.jsx";
+
+function Page() {
+  return (
+    <div>
+      <Create />
+      <Favorite />
+      <History />
+    </div>
+  );
+}
+
+export default Page;
